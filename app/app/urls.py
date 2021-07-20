c43ec1f975f6faa51508cf849dfb5ae208a5b4e4
Marysia <maria.j.kowalska@onet.pl>
2021-07-20 20:29:10 +0200

    Added working list tag endpoint

--- app/app/urls.py
+++ app/app/urls.py
@@ -5,4 +5,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('user.urls')),
+    path('api/recipe/', include('recipe.urls')),
 ]
